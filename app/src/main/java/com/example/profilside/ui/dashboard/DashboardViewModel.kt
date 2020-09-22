0aa56316b6e15b6f25a8be4c294a991a0fe7b0c3
Snailedlt <43886029+Snailedlt@users.noreply.github.com>
2020-09-22 16:58:42 +0200

    First Commit

---
+++ app/src/main/java/com/example/profilside/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.profilside.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+
+
+}
