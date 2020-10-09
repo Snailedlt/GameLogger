2fdf5f644835c185fda187da9974d9b9999f5751
Snailedlt <43886029+Snailedlt@users.noreply.github.com>
2020-10-09 18:54:00 +0200

    -Fixed "Up button" on settings and about pages.

--- app/src/main/java/com/example/profilside/MainActivity.kt
+++ app/src/main/java/com/example/profilside/MainActivity.kt
@@ -5,14 +5,13 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.onNavDestinationSelected
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration2: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +25,13 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_gameslist, R.id.navigation_profile, R.id.navigation_gamesearch))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController2 = navHostFragment.navController
+        appBarConfiguration2 = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController2, appBarConfiguration2)
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -39,5 +45,11 @@ class MainActivity : AppCompatActivity() {
         // Handle item selection
         return item.onNavDestinationSelected(findNavController(R.id.nav_host_fragment))
                 || super.onOptionsItemSelected(item)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration2)
+                || super.onSupportNavigateUp()
     }
 }
