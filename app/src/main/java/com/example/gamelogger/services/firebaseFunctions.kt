c30a04b57c72616b26d584664f58da8169f7ef8f
Larssen <46922426+Larssen@users.noreply.github.com>
2020-10-19 17:59:31 +0200

    Lagring av lagt til spill fortsatt litt uferdig
    Lagrer spill man legger til på add game i firestore

---
+++ app/src/main/java/com/example/gamelogger/services/firebaseFunctions.kt
@@ -0,0 +1,46 @@
+package com.example.gamelogger.services
+
+import android.content.Intent
+import android.util.Log
+import android.widget.Button
+import androidx.core.content.ContextCompat.startActivity
+import com.example.gamelogger.LoginActivity
+import com.example.gamelogger.MainActivity
+import com.example.gamelogger.R
+import com.example.gamelogger.ui.profile.ProfileFragment.Companion.TAG
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.fragment_login_screen.*
+
+fun addSavedGame(spillid: String, spillstate: String) {
+
+    val db = FirebaseFirestore.getInstance()
+    val auth = FirebaseAuth.getInstance()
+
+    val uid = auth.currentUser!!.uid
+
+    val docRef = db.collection("savedGames").document(uid)
+
+    val nestedData = hashMapOf(
+        "spill id" to spillid,
+        "spill state" to spillstate
+    )
+
+    nestedData[spillid] = nestedData.toString()
+
+    docRef.get()
+        .addOnSuccessListener { document ->
+            if (document != null) {
+                db.collection("savedGames").document(uid)
+                    .update(nestedData as Map<String, Any>)
+                Log.d(TAG, "Added game to firebase?")
+            } else {
+                db.collection("savedGames").document(uid)
+                    .set(nestedData)
+                Log.d(TAG, "Added more game to firebase?")
+            }
+        }
+        .addOnFailureListener { exception ->
+            Log.d(TAG, "get failed with ", exception)
+        }
+}
