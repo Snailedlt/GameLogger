c08dd4d983adbc9cf0e82267b7c46bf0216d1ea0
Snailedlt <43886029+Snailedlt@users.noreply.github.com>
2020-10-19 13:47:17 +0200

    - added searchbar to "Add Game" page

--- app/src/main/java/com/example/gamelogger/ui/gamesearch/GamesearchFragment.kt
+++ app/src/main/java/com/example/gamelogger/ui/gamesearch/GamesearchFragment.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
+import com.example.gamelogger.R
 import com.example.gamelogger.classes.Game
 import com.example.gamelogger.databinding.FragmentGamesearchBinding
 import com.example.gamelogger.ui.mygamelist.GamelistAdapter
@@ -20,6 +22,8 @@ class GamesearchFragment : Fragment() {
     private val viewModel: GamesearchViewModel by lazy {
         ViewModelProvider(this).get(GamesearchViewModel::class.java)
     }
+
+    private lateinit var searchView: SearchView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,7 +39,11 @@ class GamesearchFragment : Fragment() {
 
         binding.gamesearchlist.adapter = SearchListAdapter(SearchListAdapter.OnClickListener {
             viewModel.saveGame(it)
+
         })
+
+        searchView = binding.root.findViewById(R.id.searchBar)
+        searchView.setOnClickListener { searchView.isIconified = false }
 
         return binding.root
     }
