d03074c747e918a4a5dfcefdf298676edfa39e36
Snailedlt <43886029+Snailedlt@users.noreply.github.com>
2020-11-29 17:10:48 +0100

    - Removed unused methods

--- app/src/main/java/com/example/gamelogger/helpers/Utils.kt
+++ app/src/main/java/com/example/gamelogger/helpers/Utils.kt
@@ -19,27 +19,6 @@ fun <T> listToPresentableString(list: List<T>?) : String {
     return string.substring(0, string.length - 2)
 }
 
-
-/**
- * Uses the context, and a pixel value, to convert into a Density-independent Pixel(dp) value
- * Code from: https://stackoverflow.com/a/12147550/12206312
- *
- * Not used anywhere in the current app, but it was used during testing for some fragments
- */
-//fun dpFromPx(context: Context, px: Float): Float {
-//    return px / context.resources.displayMetrics.density
-//}
-
-/**
- * Uses the context, and a Density-independent Pixel(dp) value, to convert into a pixel value
- * Code from: https://stackoverflow.com/a/12147550/12206312
- *
- * Not used anywhere in the current app, but it was used during testing for some fragments
- */
-//fun pxFromDp(context: Context, dp: Float): Float {
-//    return dp * context.resources.displayMetrics.density
-//}
-
 /**
  * Takes a hashcode: Int as parameter,
  * and turns it into an Hexcode: String with the format RRGGBB
